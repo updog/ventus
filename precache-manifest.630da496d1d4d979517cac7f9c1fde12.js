0175cfbf1d2c2de69a94c8058e616be48db2bd62
Christopher Trinh <ctrinh@bu.edu>
2018-11-18 22:44:06 -0500

    Updates

---
+++ precache-manifest.630da496d1d4d979517cac7f9c1fde12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb24d885bf2649114977",
+    "url": "/ventus/static/js/runtime~main.eb24d885.js"
+  },
+  {
+    "revision": "e0b98f9ad2eee50d555c",
+    "url": "/ventus/static/js/main.e0b98f9a.chunk.js"
+  },
+  {
+    "revision": "890813d2e221c6ea3824",
+    "url": "/ventus/static/js/1.890813d2.chunk.js"
+  },
+  {
+    "revision": "e0b98f9ad2eee50d555c",
+    "url": "/ventus/static/css/main.4236cb67.chunk.css"
+  },
+  {
+    "revision": "b96f053649ced238430b9beee8c84f1f",
+    "url": "/ventus/index.html"
+  }
+];
